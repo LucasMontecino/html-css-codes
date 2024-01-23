242e2f0bfcda29bdc3796f76ca93e66f15f932d3
Lucas Montecino <lucasmontecino95@gmail.com>
2024-01-23 09:36:55 -0300

    fit-img

--- for-practice/script.js
+++ for-practice/script.js
@@ -3,11 +3,13 @@ const gallery = [
     name: "Ronaldinho",
     nacionality: "Brazil",
     foot: "right",
+    img: "./assets/images/image-gallery-2.jpeg",
   },
   {
     name: "Figo",
     nacionality: "Portugal",
     foot: "right",
+    img: "./assets/images/image-gallery-1.jpeg",
   },
 ];
 
@@ -16,6 +18,7 @@ const container = document.querySelector(".gallery");
 gallery.forEach((el) => {
   container.innerHTML += `
         <div class="gallery-card">
+            <img src="${el.img}" alt="${el.name}"
             <h2>Player: ${el.name}</h2>
             <p>Nation: ${el.nacionality}</p>
             <p>Habil Foot: ${el.foot}</p>
