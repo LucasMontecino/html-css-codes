9e8e1a527891c787aaa311af89e5bb765769c685
Lucas Montecino <lucasmontecino95@gmail.com>
2024-02-06 14:44:55 -0300

    add-script

---
+++ for-practice/script.js
@@ -0,0 +1,30 @@
+const gallery = [
+  {
+    id: 1,
+    title: "",
+    description: "",
+    category: "",
+    img: "",
+  },
+  {
+    id: 2,
+    title: "",
+    description: "",
+    category: "",
+    img: "",
+  },
+  {
+    id: 3,
+    title: "",
+    description: "",
+    category: "",
+    img: "",
+  },
+  {
+    id: 4,
+    title: "",
+    description: "",
+    category: "",
+    img: "",
+  },
+];
